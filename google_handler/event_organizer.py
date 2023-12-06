682108c7b6271b134150a8c0699150c8efc9a473
AdamWier <38018787+AdamWier@users.noreply.github.com>
2023-12-06 11:15:06 +0100

    Remove emoji from titles

--- google_handler/event_organizer.py
+++ google_handler/event_organizer.py
@@ -1,13 +1,15 @@
 from datetime import datetime
 from functools import reduce
 
+from emoji import replace_emoji
+
 
 def create_event_day_dictionary(events):
     return reduce(event_organizer, events, {})
 
 
 def event_organizer(dictionary, event):
-    key = event.get("summary")
+    key = replace_emoji(event.get("summary"), replace="").strip()
     daysToAdd = dictionary.get(key, [])[:]
     dayString = event.get("start").get("dateTime")
     weekdayIndex = datetime.strptime(dayString, "%Y-%m-%dT%H:%M:%S%z").weekday()
--- habitica_handler/daily_organizer.py
+++ habitica_handler/daily_organizer.py
@@ -1,12 +1,14 @@
 from functools import reduce
 
+from emoji import replace_emoji
+
 
 def create_day_by_daily_dictionary(dailies):
     return reduce(daily_organizer, dailies, {})
 
 
 def daily_organizer(dictionary, daily):
-    key = daily.get("text")
+    key = replace_emoji(daily.get("text"), replace="").strip()
     daysToAdd = dictionary.get(key, [])[:]
     days = list(
         filter(lambda day: daily.get("repeat").get(day), daily.get("repeat").keys())
