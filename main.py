6968bce681b6ea19db5cd1aa2cefb7c5e671f81e
AdamWier <38018787+AdamWier@users.noreply.github.com>
2023-12-09 14:07:26 +0100

    Send multiple update requests

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from dotenv import load_dotenv
 
 from get_information import get_information
 from request_preparer.request_preparer import request_preparer
+from utils.send_multiple_requests import send_multiple_requests
 
 load_dotenv()
 
@@ -10,7 +11,13 @@ load_dotenv()
 def main():
     information = get_information()
 
-    request_preparer(information["googleEvents"], information["habiticaTasks"])
+    request_information = request_preparer(
+        information["googleEvents"], information["habiticaTasks"]
+    )
+
+    responses = send_multiple_requests(request_information.get("updateRequests"))
+
+    print(responses)
 
 
 if __name__ == "__main__":
---
+++ utils/send_multiple_requests.py
@@ -0,0 +1,24 @@
+import os
+from concurrent.futures import ThreadPoolExecutor
+
+import requests
+
+
+def send_multiple_requests(request_information):
+    HABITICA_USER_ID = os.getenv("HABITICA_USER_ID")
+    HABITICA_API_KEY = os.getenv("HABITICA_API_KEY")
+
+    def send_requests(request_information):
+        response = requests.put(
+            request_information.get("url"),
+            headers={
+                "x-api-user": HABITICA_USER_ID,
+                "x-api-key": HABITICA_API_KEY,
+            },
+            json=request_information.get("params"),
+        )
+
+        return response
+
+    with ThreadPoolExecutor(max_workers=3) as pool:
+        return tuple(pool.map(send_requests, request_information))
